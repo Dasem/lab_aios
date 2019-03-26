d4acd15688bfdcde9398613e2abd5108ab0c2a65
Tararkov Andrey <gfhflbuvf46@mail.ru>
2019-03-26 19:55:09 +0500

    + парсер

---
+++ parser.py
@@ -0,0 +1,26 @@
+def printishe(matrix):
+    n = len(matrix)
+    for i in range(n):
+        print('[', end='')
+        for j in range(n):
+            if j != n - 1:
+                print(str(matrix[i][j]) + ', ', end='')
+            else:
+                print(str(matrix[i][j]), end='')
+        if i != n - 1:
+            print('],')
+        else:
+            print(']')
+
+
+file = open('input.txt', 'r')
+rows = file.readlines()
+result_matrix = []
+for row in rows:
+    new_row = row.split('\t')
+    new_row[-1] = new_row[-1].strip()
+    result_matrix.append(new_row)
+for i in range(len(result_matrix)):
+    for j in range(len(result_matrix)):
+        result_matrix[j][i] = result_matrix[i][j]
+printishe(result_matrix)
